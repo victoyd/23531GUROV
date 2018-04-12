1d3e730cd3cd065fcd7d2eb03de1faef0ff3d803
Victor Gurov <gurovic1996@gmail.com>
2018-04-12 22:18:57 +0300

    added input validation in task 1

--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -6,17 +6,29 @@ public class Task1 {
   public static void main(final String[] args) {
     // опрашиваем ввод
     final Scanner scanner = new Scanner(System.in);
-    System.out.print("a = ");
-    final double a = scanner.nextDouble();
-    System.out.print("b = ");
-    final double b = scanner.nextDouble();
-    System.out.print("h = ");
-    final double h = scanner.nextDouble();
+    final double a = tryReadDouble("a", scanner);
+    final double b = tryReadDouble("b", scanner);
+    final double h = tryReadDouble("h", scanner);
     scanner.close();
     // выводим результат функции
     for (double x = a; x <= b; x += h) {
       System.out.printf("F(%g)=%g\n", x, calc(x));
     }
+  }
+
+  // здесь производится валидация ввода
+  private static double tryReadDouble(final String value, final Scanner scanner) {
+    double dVal = Double.NaN;
+    do {
+      System.out.print(value + " = ");
+      if (scanner.hasNextDouble()) {
+        dVal = scanner.nextDouble();
+      } else {
+        System.out.println("Only numbers are allowed!");
+        scanner.next();
+      }
+    } while (Double.isNaN(dVal));
+    return dVal;
   }
 
   private static double calc(final double x) {
